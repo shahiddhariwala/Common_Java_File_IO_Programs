1cbacbdc4c07f45d015c222c2689d9c081ccdbec
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-03-29 14:31:34 +0530

    Added Program to Read Lines from FilesM

---
+++ Common_Java_File_IO_Programs/src/ReadingFromFile.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadingFromFile {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		FileInputStream in = new FileInputStream("src/SampleInput.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		
+		String currentLine = br.readLine();
+		while(currentLine != null)
+		{
+			System.out.println(currentLine);
+			currentLine = br.readLine();
+		}
+		br.close();
+	}
+
+}
